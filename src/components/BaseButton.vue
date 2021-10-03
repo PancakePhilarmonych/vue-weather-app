3973ab6ad0e407e7485f8f34c319358c79a572ca
Alexandr Panfilov <rikko.alexandr@gmail.com>
2021-10-03 17:25:38 +0300

    create base-button component

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <button
+    @click="onClick"
+    class="base-button">
+    {{ innerText }}
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    innerText: {
+      type: String,
+      default: 'Button'
+    }
+  },
+
+  methods: {
+    onClick () {
+      this.$emit('click')
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
