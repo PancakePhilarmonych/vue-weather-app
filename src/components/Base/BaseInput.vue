157578a8b2403a6384b9b529b6ca54988ae8b2c8
Alexandr Panfilov <rikko.alexandr@gmail.com>
2021-10-03 20:50:52 +0300

    updated base-input

--- src/components/Base/BaseInput.vue
+++ src/components/Base/BaseInput.vue
@@ -1,20 +1,44 @@
 <template>
   <input
+      v-model="copyValue"
       class="base-input"
       :type="type"
-      :name="name"
       :disabled="disabled"
-      :placeholder="placeholder"
-      @input="onInput"
-      @change="onChange">
+      :placeholder="placeholder">
 </template>
 
 <script>
 export default {
   props: {
+    value: {
+      type: String,
+      default: ''
+    },
+
     placeholder: {
       type: String,
       default: 'Search city'
+    },
+
+    type: {
+      type: String,
+      default: 'text'
+    },
+
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  computed: {
+    copyValue: {
+      get () {
+        return this.value
+      },
+      set (newValue) {
+        this.$emit('input', newValue)
+      }
     }
   }
 
@@ -22,23 +46,18 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.base-input {
-  width: 100%;
-  height: 40px;
-  outline: none;
-  border: none;
-  border-bottom: 1px solid #1B1B1B;
-  padding-bottom: 16px;
+  .base-input {
+    width: 100%;
+    height: 40px;
 
-  font-family: Lato;
-  font-style: normal;
-  font-weight: 500;
-  font-size: 24px;
-  line-height: 24px;
-  /* identical to box height, or 100% */
+    padding-bottom: 16px;
 
-  /* Text/Primary */
+    outline: none;
+    border: none;
+    border-bottom: 1px solid #1B1B1B;
 
-  color: #1B1B1B;
-}
+    font-size: 24px;
+    line-height: 24px;
+    color: #1B1B1B;
+  }
 </style>
