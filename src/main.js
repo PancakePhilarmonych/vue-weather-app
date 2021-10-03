d2c49d88c7a4aab2f2183afd0d588140d79b8587
Alexandr Panfilov <rikko.alexandr@gmail.com>
2021-10-04 00:32:13 +0300

    install axios

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import './assets/styles/main.scss'
+import initAxios from './services/axios.js'
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 
 Vue.config.productionTip = false
+
+initAxios()
 
 new Vue({
   store,
