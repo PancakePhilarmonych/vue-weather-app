090db7ba95f06392437c0fb65f8b5b5f35327a2f
Alexandr Panfilov <rikko.alexandr@gmail.com>
2021-10-03 20:51:42 +0300

    create base-header card

---
+++ src/components/Base/BaseRow.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="base-row">
+    <p class="title">{{ titleText }}</p>
+    <p class="info">{{ infoText }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    titleText: {
+      type: String,
+      required: true
+    },
+
+    infoText: {
+      type: String,
+      required: true
+    }
+  }
+
+}
+</script>
+
+<style lang="scss" scoped>
+  .base-row {
+    display: flex;
+    justify-content: space-between;
+    padding: 16px 0 12px 0;
+    border-bottom: 1px solid #C4C4C4;
+  }
+</style>
