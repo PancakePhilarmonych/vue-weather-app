cc48e268db695ba5c0aa1cbe71df4384c63b62f8
Alexandr Panfilov <rikko.alexandr@gmail.com>
2021-10-09 20:14:57 +0300

    create api requests

--- src/api.js
+++ src/api.js
@@ -1,10 +1,17 @@
 import axios from 'axios'
+
 const baseUrl = '/weather'
+const openWeatherToken = process.env.VUE_APP_OW_KEY
+const paramsWithToken = (params) => ({ params: { ...params, appid: openWeatherToken } })
 
 export default {
   async getWeatherByCity (params) {
-    const response = await axios.get(`${baseUrl}`, { params: { ...params } })
+    const response = await axios.get(`${baseUrl}`, paramsWithToken(params))
+    return response
+  },
 
+  async getWeatherByPosition (params) {
+    const response = await axios.get(`${baseUrl}`, paramsWithToken(params))
     return response
   }
 }
