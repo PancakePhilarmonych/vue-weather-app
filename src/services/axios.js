5066df7a907a395553df4276a3596432c7290313
Alexandr Panfilov <rikko.alexandr@gmail.com>
2021-10-09 20:16:23 +0300

    complete axios init function

--- src/services/axios.js
+++ src/services/axios.js
@@ -2,10 +2,7 @@ import axios from 'axios'
 
 const init = () => {
   axios.defaults.responseType = 'json'
-
   axios.defaults.baseURL = process.env.VUE_APP_BASE_URL
-  axios.defaults.headers['x-rapidapi-key'] = process.env.VUE_APP_OW_TOKEN
-  axios.defaults.headers['x-rapidapi-host'] = process.env.VUE_APP_OW_HOST
 }
 
 export default init
